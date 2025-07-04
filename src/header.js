0ce6fac9ee16a68b3e32d3b48b16b255ca6cf357
Puncher <puncher@gmail.com>
2025-07-04 04:18:02 +0300

    chore: Bump version to 1.2.8 in header and user script

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Empowerment
 // @namespace    klavogonki
-// @version      1.2.7 
+// @version      1.2.8 
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/g*
